b00f3eed192e1dcac94722e4b341f9cfd1e8766d
Vincent Olagbemide <vincentolagbemide@Vincents-MacBook-Pro.local>
2019-06-19 13:22:48 +0100

    update readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const Header = props => {
+  console.log(props);
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
+    </header>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button> - </button>
+      <span className="counter-score">35</span>
+      <button> + </button>
+    </div>
+  );
+};
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Vinay</span>
+      <Counter />
+    </div>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Scoreboard" totalPlayers={1} />
+      {/* Player list */}
+      <Player />
     </div>
   );
 }
