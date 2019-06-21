c27ad53f901f321541b696a9ae51e061f36b2037
Vincent Olagbemide <vincentolagbemide@Vincents-MacBook-Pro.local>
2019-06-21 08:51:41 +0100

    add app.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const Header = props => {
-  console.log(props);
   return (
     <header>
       <h1>{props.title}</h1>
@@ -12,32 +10,38 @@ const Header = props => {
   );
 };
 
-const Counter = () => {
+const Counter = props => {
   return (
     <div className="counter">
-      <button> - </button>
-      <span className="counter-score">35</span>
-      <button> + </button>
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">{props.score}</span>
+      <button className="counter-action increment"> + </button>
     </div>
   );
 };
 
-const Player = () => {
+const Player = ({ name, score }) => {
   return (
     <div className="player">
-      <span className="player-name">Vinay</span>
-      <Counter />
+      <span className="player-name">{name}</span>
+      <Counter score={score} />
     </div>
   );
 };
-function App() {
+const App = props => {
   return (
-    <div>
-      <Header title="Scoreboard" totalPlayers={1} />
-      {/* Player list */}
-      <Player />
+    <div className="scoreboard">
+      <Header title="Scoreboard" totalPlayers={props.initialPlayers.length} />
+      {/* Player list  */}
+      {props.initialPlayers.map(player => (
+        <Player
+          key={player.id.toString()}
+          name={player.name}
+          score={player.score}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+const players = [
+  { id: 1, name: "Vinay", score: 50 },
+  { id: 2, name: "Tolu", score: 30 },
+  { id: 3, name: "Maleek", score: 40 },
+  { id: 4, name: "Abass", score: 35 }
+];
+ReactDOM.render(
+  <App initialPlayers={players} />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
