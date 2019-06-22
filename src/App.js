cbbeb10aa623245c1bb7bbf43f1d0c1a6d7f6de5
vincentayorinde <vincentayorinde@yahoo.com>
2019-06-22 10:17:32 +0100

    add inrement/decrement

--- src/App.js
+++ src/App.js
@@ -1,44 +1,71 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 const Header = props => {
   return (
     <header>
       <h1>{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
+      <span className='stats'>Players: {props.totalPlayers}</span>
     </header>
   );
 };
 
-const Counter = props => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{props.score}</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-};
+class Counter extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     score: 0
+  //   };
+  // }
+  state = {
+    score: 0
+  };
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+  };
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1
+    });
+  };
+  render() {
+    return (
+      <div className='counter'>
+        <button
+          className='counter-action decrement'
+          onClick={this.decrementScore}
+        >
+          -
+        </button>
+        <span className='counter-score'>{this.state.score}</span>
+        <button
+          className='counter-action increment'
+          onClick={this.incrementScore}
+        >
+          +
+        </button>
+      </div>
+    );
+  }
+}
 
 const Player = ({ name, score }) => {
   return (
-    <div className="player">
-      <span className="player-name">{name}</span>
-      <Counter score={score} />
+    <div className='player'>
+      <span className='player-name'>{name}</span>
+      <Counter />
     </div>
   );
 };
 const App = props => {
   return (
-    <div className="scoreboard">
-      <Header title="Scoreboard" totalPlayers={props.initialPlayers.length} />
+    <div className='scoreboard'>
+      <Header title='Scoreboard' totalPlayers={props.initialPlayers.length} />
       {/* Player list  */}
       {props.initialPlayers.map(player => (
-        <Player
-          key={player.id.toString()}
-          name={player.name}
-          score={player.score}
-        />
+        <Player key={player.id.toString()} name={player.name} />
       ))}
     </div>
   );
